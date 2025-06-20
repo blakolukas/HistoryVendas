2a0d7a0d81abbd1c1f919c7e18deab7cddd1dc17
blakolukas <lucasblacovicz@gmail.com>
2025-06-20 12:57:26 -0300

    first commit

---
+++ src/main/java/com/projarq/historyservice/HistoryApplication.java
@@ -0,0 +1,13 @@
+package com.projarq.historyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryApplication.class, args);
+	}
+
+}
